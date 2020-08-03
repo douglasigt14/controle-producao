92f7e8547bae4e565730981c0ba71d236ef93ea8
douglasigt14 <55840011+douglasigt14@users.noreply.github.com>
2020-08-03 10:49:06 -0300

    Botoês modificados

--- components/GrupoButoes.js
+++ components/GrupoButoes.js
@@ -4,11 +4,7 @@ import styled from "styled-components";
 import { Button } from 'react-native-paper';
 
 const Texto = styled.Text`
-    font-size: 15px;
-  `;
-
-const But = styled.Button`
-    font-size: 15px;
+    font-size: 18px;
   `;
 
 const Div = styled.View`
@@ -18,11 +14,14 @@ const Div = styled.View`
     padding-right: 20px;
     padding-top: 10px;
     justify-content: space-around;
+    height: 80px;
 `;
+
+
 
 export default () => {
   const [msg, setMsg] = useState('Douglas');
-  let finalizado = true;
+  let finalizado = false;
   return (
     <Div>
       <Div>
@@ -37,12 +36,12 @@ export default () => {
       </Div>
       <Div>
         <Button color="#007bff" mode="contained" onPress={() => console.log('Pressed')}>
-          <Texto>Controles Diarios </Texto>
+          <Texto>C. Diarios </Texto>
         </Button>
       </Div>
       <Div>
         <Button color="#ffc107" mode="contained" onPress={() => console.log('Pressed')}>
-          <Texto>Tipos de Paradas</Texto>
+          <Texto>T. de Paradas</Texto>
         </Button>
       </Div>
       {finalizado == false ? (
@@ -52,7 +51,11 @@ export default () => {
           </Button>
         </Div>
       ) : (
-        false
+          <Div>
+            <Button color="#6c757d" mode="contained" disabled='true' onPress={() => console.log('Pressed')}>
+              <Texto>Finalizar</Texto>
+            </Button>
+          </Div>
       )}
     </Div>
   );
