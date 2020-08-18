5e25eebc59546c8ece2cbedb23b9be25f2f53d49
douglasigt14 <55840011+douglasigt14@users.noreply.github.com>
2020-08-18 08:08:48 -0300

    Busca no Storage dos dados novos

--- App.js
+++ App.js
@@ -26,6 +26,8 @@ export default  function App() {
       };
       buscar_storage("@id_posto", setId_posto);
       buscar_storage("@logado", setLogado);
+      buscar_storage("@operador_id", setOperador_id);
+      buscar_storage("@operador_desc", setOperador_desc);
     }, []);
 
   const selecionar_posto = (id_postoP) => {
