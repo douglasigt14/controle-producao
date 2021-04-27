ea21b0ee7576a97eb3113ff8c12e20969792217c
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-27 11:59:16 -0300

    Tirei Botões + e - do PDF

--- components/PdfView.js
+++ components/PdfView.js
@@ -8,15 +8,15 @@ export default (props) => {
       <PDFReader
       withPinchZoom={true}
       style={{marginTop: 50}}
-    //   customStyle={{
+      customStyle={{
     //     readerContainer: {padding: 0,margin: 0},
     //     readerContainerDocument: {margin: 10,padding: 0},
     //     readerContainerNumbers: {margin: 0,display: 'none'},
     //     readerContainerNumbersContent: {display: 'none'},
-    //     readerContainerZoomContainerButton: {display: 'none'},
+           readerContainerZoomContainerButton: {display: 'none'}
     //     readerContainerZoomContainer: {display: 'none'},
     //     readerContainerNavigate: {display: 'none'}
-    //   }}
+      }}
         source={{
           uri: url,
         }}
